237b7dbe1cc7fd2bc7d34e76566b0679d5c842c7
alexge50 <alexge50.x@gmail.com>
2019-09-23 19:41:19 +0300

    replaced sfml with sdl2 + opengl

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,45 @@
-extern crate sfml;
-
-use sfml as sf;
+extern crate sdl2;
+extern crate glm;
+extern crate gl;
 
 fn main() {
-    let settings = sf::window::ContextSettings {
-        depth_bits: 24,
-        stencil_bits: 8,
-        antialiasing_level: 4,
-        major_version: 3,
-        minor_version: 0,
-        attribute_flags: 1,
-        srgb_capable: 0
-    };
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let gl_attr = video_subsystem.gl_attr();
 
-    let mut window = sf::graphics::RenderWindow::new(
-        (512, 512),
-        "sphere",
-        sf::window::Style::TITLEBAR | sf::window::Style::CLOSE,
-        &settings,
-    );
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 0);
 
-    window.set_framerate_limit(60);
-    while window.is_open() {
-        while let Some(event) = window.poll_event() {
+    let window = video_subsystem
+        .window("sphere", 512, 512)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
 
+    let gl_context = window.gl_create_context().unwrap();
+    let gl_ =
+        gl::load_with(
+            |s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
+        );
+
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main_loop: loop{
+        for event in event_pump.poll_iter() {
             match event {
-                sfml::window::Event::Closed => window.close(),
+                sdl2::event::Event::Quit {..} => break 'main_loop,
                 _ => {}
             }
         }
 
-        window.display();
-    }
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
 
-    println!("Hello, world!");
+        window.gl_swap_window();
+    }
 }
