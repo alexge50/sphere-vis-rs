96264efb1883311f1ad4ec5c0e40ae40c397f00b
alexge50 <alexge50.x@gmail.com>
2019-09-24 19:23:50 +0300

    drawing sphere

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate sdl2;
 extern crate glm;
 extern crate gl;
 
+mod shader;
+mod sphere;
+
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
@@ -27,6 +30,75 @@ fn main() {
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
+    let vertex_shader = shader::Shader::from_source(
+        include_str!("shaders/vertex.glsl"),
+        gl::VERTEX_SHADER
+    ).unwrap();
+
+    let fragment_shader = shader::Shader::from_source(
+        include_str!("shaders/fragment.glsl"),
+        gl::FRAGMENT_SHADER
+    ).unwrap();
+
+    let shader_program = shader::Program::from_shaders(
+        &[vertex_shader, fragment_shader]
+    ).unwrap();
+
+    let sphere = sphere::Sphere::generate(
+        1.0,
+        10 ,
+        10
+    );
+
+    let mut vbo: gl::types::GLuint = 0;
+    let mut ebo: gl::types::GLuint = 0;
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (sphere.vertices.len() * std::mem::size_of::<f32>()) as isize,
+            sphere.vertices.as_ptr() as *const gl::types::GLvoid,
+            gl::STATIC_DRAW
+        );
+
+        gl::GenBuffers(1, &mut ebo);
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            (sphere.indices.len() * std::mem::size_of::<i32>()) as isize,
+            sphere.indices.as_ptr() as *const gl::types::GLvoid,
+            gl::STATIC_DRAW
+        );
+
+        gl::GenVertexArrays(1, &mut vao);
+
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (3 * std::mem::size_of::<f32>()) as i32,
+            std::ptr::null()
+        );
+
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+
+
+        gl::BindVertexArray(0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, 0);
+    }
+
+    unsafe {
+        gl::Enable(gl::DEPTH_TEST);
+    }
+
     let mut event_pump = sdl.event_pump().unwrap();
     'main_loop: loop{
         for event in event_pump.poll_iter() {
@@ -37,7 +109,19 @@ fn main() {
         }
 
         unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        }
+
+        shader_program.set_used();
+
+        unsafe {
+            gl::BindVertexArray(vao);
+            gl::DrawElements(
+                gl::LINE_STRIP,
+                sphere.indices.len() as i32,
+                gl::UNSIGNED_INT,
+                std::ptr::null()
+            );
         }
 
         window.gl_swap_window();
